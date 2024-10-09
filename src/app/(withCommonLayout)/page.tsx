12007e4b6cf3e904327a2460f1f9e748891327ac
Azir@9200 <aziruddin83@hotmail.com>
2024-10-10 00:28:47 +0200

    created index file

---
+++ src/app/(withCommonLayout)/page.tsx
@@ -0,0 +1,11 @@
+import Landing from "@/src/components/home/Landing";
+import RecentPosts from "@/src/components/home/RecentPosts";
+
+export default function Home() {
+  return (
+    <>
+      <Landing />
+      <RecentPosts />
+    </>
+  );
+}
